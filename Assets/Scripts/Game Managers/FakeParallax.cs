77d2a4064b040f242a66c064c5c1a5fc6f2d6580
Leenton <>
2021-06-02 22:00:13 +0100

    Added leet code and images, made a background that is very cool, struggling with buttons as unity does not register clicks. Medium sad.

---
+++ Assets/Scripts/Game Managers/FakeParallax.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeParallax : MonoBehaviour
+{
+    private float height, startPosition;
+    private float distanceTraveled = 0;
+    public float starSpeed;
+    
+    void Start()
+    {
+        startPosition = transform.position.y;
+        height = transform.GetComponent<RectTransform> ().sizeDelta.y;
+    }
+
+    void Update()
+    {
+         if(distanceTraveled >= height)
+         {
+             transform.position = new Vector3(transform.position.x, startPosition, transform.position.z);
+             distanceTraveled = 0;
+         }else
+         {
+             //transform.position = new Vector3(transform.position.x, startPosition + starSpeed, transform.position.z);
+             transform.Translate(Vector3.down * (Time.deltaTime * starSpeed));
+             distanceTraveled +=  starSpeed * Time.deltaTime; 
+         }
+    }
+}
---
+++ Assets/Scripts/Game Managers/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public static void Play()
+    {   
+        print("This is a test to make sure clicks work");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level1");
+    }
+
+    public static void Quit()
+    {
+        print("This is a test to make sure clicks work");
+        Application.Quit();
+    }
+    public static void Score()
+    {
+        print("This is a test to make sure clicks work");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Score");
+    }
+}
